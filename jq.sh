81a26d71b98aa5dbe845b5e2e95260f72432c863
valfur03 <valentin.furmanek@gmail.com>
2021-10-23 11:48:33 +0200

    fix: add quotes around PATH variable

--- jq.sh
+++ jq.sh
@@ -21,7 +21,7 @@ printf "Now add the following lines to your .zshrc\n"
 printf "(or .bashrc or whatever file your shell uses).\n"
 printf "
 if [ -d \$HOME/.local/usr/bin ]; then
-    export PATH="\$HOME/.local/usr/bin:\$PATH"
+    export PATH=\"\$HOME/.local/usr/bin:\$PATH\"
 fi\n\n"
 
 printf "${YELLOW}You may need to restart your shell\n"
