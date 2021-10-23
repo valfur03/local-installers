99ce8d26add754e1f2c8b0004830efb3b4e95335
valfur03 <valentin.furmanek@gmail.com>
2021-10-23 12:01:51 +0200

    feat: print help path only if the PATH variable doesn't contain the command's path

--- utils/print_help_path.sh
+++ utils/print_help_path.sh
@@ -2,11 +2,15 @@
 
 print_help_path()
 {
-	printf "${GREEN}jq is installed!${NC}\n"
-	printf "Now add the following lines to your .zshrc\n"
-	printf "(or .bashrc or whatever file your shell uses).\n"
-	printf "
+	if ! echo $PATH | grep "$HOME/.local/usr/bin" > /dev/null 2>&1 \
+		&& ! echo $PATH | grep '$HOME/.local/usr/bin' > /dev/null 2>&1
+	then
+		printf "${GREEN}jq is installed!${NC}\n"
+		printf "Now add the following lines to your .zshrc\n"
+		printf "(or .bashrc or whatever file your shell uses).\n"
+		printf "
 if [ -d \$HOME/.local/usr/bin ]; then
     export PATH=\"\$HOME/.local/usr/bin:\$PATH\"
 fi\n\n"
+	fi
 }
