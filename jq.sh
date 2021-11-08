d69128556167005dbb80d6398dc979c8900a8543
valfur03 <valentin.furmanek@gmail.com>
2021-11-08 14:39:20 +0100

    refactor: use single error variable

--- jq.sh
+++ jq.sh
@@ -10,9 +10,7 @@ PACKAGE_NAME="jq"
 PACKAGE_VERSION="1.5"
 PACKAGE_SOURCE="https://github.com/stedolan/jq/releases/download/jq-$PACKAGE_VERSION/jq-linux64"
 
-CURL_ERROR=""
-CHMOD_ERROR=""
-MKDIR_ERROR=""
+ERROR=""
 
 if ! commands_exist curl
 then
@@ -20,37 +18,37 @@ then
 fi
 
 printf "${BLUE}Downloading %s...${NC}\n" "$PACKAGE_NAME"
-if ! CURL_ERROR=$(curl -fsSL -o $PACKAGE_NAME $PACKAGE_SOURCE 2>&1)
+if ! ERROR=$(curl -fsSL -o $PACKAGE_NAME $PACKAGE_SOURCE 2>&1)
 then
 	printf "${RED}%s could not be downloaded...${NC}\n" $PACKAGE_NAME
 	print_help_issue
-	print_debug_info "$CURL_ERROR"
+	print_debug_info "$ERROR"
 	exit 1
 fi
 
-if ! CHMOD_ERROR=$(chmod +x $PACKAGE_NAME 2>&1)
+if ! ERROR=$(chmod +x $PACKAGE_NAME 2>&1)
 then
 	printf "${RED}Could not give execution permissions to %s...${NC}\n" $PACKAGE_NAME
 	print_help_issue
-	print_debug_info "$CHMOD_ERROR"
+	print_debug_info "$ERROR"
 	rm $PACKAGE_NAME
 	exit 1
 fi
 
 printf "${BLUE}Installing %s in \$HOME/.local/usr/bin...${NC}\n" $PACKAGE_NAME
-if ! MKDIR_ERROR=$(mkdir -p $HOME/.local/usr/bin 2>&1)
+if ! ERROR=$(mkdir -p $HOME/.local/usr/bin 2>&1)
 then
 	printf "${RED}Could not create folder $HOME/.local/usr/bin...${NC}\n"
 	print_help_issue
-	print_debug_info "$MKDIR_ERROR"
+	print_debug_info "$ERROR"
 	rm $PACKAGE_NAME
 	exit 1
 fi
-if ! MV_ERROR=$(mv jq $HOME/.local/usr/bin 2>&1)
+if ! ERROR=$(mv jq $HOME/.local/usr/bin 2>&1)
 then
 	printf "${RED}Could not mv %s $HOME/.local/usr/bin...${NC}\n" $PACKAGE_NAME
 	print_help_issue
-	print_debug_info "$MV_ERROR"
+	print_debug_info "$ERROR"
 	rm $PACKAGE_NAME
 	exit 1
 fi
--- node.sh
+++ node.sh
@@ -12,10 +12,7 @@ PACKAGE_DIRECTORY="node-v$PACKAGE_VERSION-linux-x64"
 PACKAGE_ARCHIVE="$PACKAGE_DIRECTORY.tar.xz"
 PACKAGE_SOURCE="https://nodejs.org/dist/v$PACKAGE_VERSION/node-v$PACKAGE_VERSION-linux-x64.tar.xz"
 
-CURL_ERROR=""
-CHMOD_ERROR=""
-MKDIR_ERROR=""
-TAR_ERROR=""
+ERROR=""
 
 if ! commands_exist curl
 then
@@ -23,46 +20,46 @@ then
 fi
 
 printf "${BLUE}Downloading %s...${NC}\n" "$PACKAGE_NAME"
-if ! CURL_ERROR=$(curl -fsSL -o $PACKAGE_ARCHIVE $PACKAGE_SOURCE 2>&1)
+if ! ERROR=$(curl -fsSL -o $PACKAGE_ARCHIVE $PACKAGE_SOURCE 2>&1)
 then
 	printf "${RED}%s could not be downloaded...${NC}\n" $PACKAGE_NAME
 	print_help_issue
-	print_debug_info "$CURL_ERROR"
+	print_debug_info "$ERROR"
 	exit 1
 fi
 
 printf "${BLUE}Extracting %s arhive in %s...${NC}\n" $PACKAGE_NAME $PACKAGE_DIRECTORY
-if ! TAR_ERROR=$(tar xf $PACKAGE_ARCHIVE 2>&1)
+if ! ERROR=$(tar xf $PACKAGE_ARCHIVE 2>&1)
 then
 	printf "${RED}Could not extract %s...${NC}\n" $PACKAGE_ARCHIVE
 	print_help_issue
-	print_debug_info "$TAR_ERROR"
+	print_debug_info "$ERROR"
 	rm -r $PACKAGE_DIRECTORY
 	rm $PACKAGE_ARCHIVE
 	exit 1
 fi
 
 printf "${BLUE}Installing %s in \$HOME/.local/usr/bin...${NC}\n" $PACKAGE_NAME
-if ! MKDIR_ERROR=$(mkdir -p $HOME/.local/usr/bin 2>&1) \
-	|| ! MKDIR_ERROR=$(mkdir -p $HOME/.local/usr/include 2>&1) \
-	|| ! MKDIR_ERROR=$(mkdir -p $HOME/.local/usr/lib 2>&1) \
-	|| ! MKDIR_ERROR=$(mkdir -p $HOME/.local/usr/share 2>&1)
+if ! ERROR=$(mkdir -p $HOME/.local/usr/bin 2>&1) \
+	|| ! ERROR=$(mkdir -p $HOME/.local/usr/include 2>&1) \
+	|| ! ERROR=$(mkdir -p $HOME/.local/usr/lib 2>&1) \
+	|| ! ERROR=$(mkdir -p $HOME/.local/usr/share 2>&1)
 then
 	printf "${RED}Could not create in folder $HOME/.local/usr...${NC}\n"
 	print_help_issue
-	print_debug_info "$MKDIR_ERROR"
+	print_debug_info "$ERROR"
 	rm -r $PACKAGE_DIRECTORY
 	rm $PACKAGE_ARCHIVE
 	exit 1
 fi
-if ! CP_ERROR=$(cp -rf $PACKAGE_DIRECTORY/bin $HOME/.local/usr 2>&1) \
-	|| ! CP_ERROR=$(cp -rf $PACKAGE_DIRECTORY/include $HOME/.local/usr 2>&1) \
-	|| ! MV_ERROR=$(cp -rf $PACKAGE_DIRECTORY/lib $HOME/.local/usr 2>&1) \
-	|| ! MV_ERROR=$(cp -rf $PACKAGE_DIRECTORY/share $HOME/.local/usr 2>&1)
+if ! ERROR=$(cp -rf $PACKAGE_DIRECTORY/bin $HOME/.local/usr 2>&1) \
+	|| ! ERROR=$(cp -rf $PACKAGE_DIRECTORY/include $HOME/.local/usr 2>&1) \
+	|| ! ERROR=$(cp -rf $PACKAGE_DIRECTORY/lib $HOME/.local/usr 2>&1) \
+	|| ! ERROR=$(cp -rf $PACKAGE_DIRECTORY/share $HOME/.local/usr 2>&1)
 then
 	printf "${RED}Could not cp %s $HOME/.local...${NC}\n" $PACKAGE_NAME
 	print_help_issue
-	print_debug_info "$MV_ERROR"
+	print_debug_info "$ERROR"
 	rm -r $PACKAGE_DIRECTORY
 	rm $PACKAGE_ARCHIVE
 	exit 1
